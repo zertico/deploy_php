381c65395a0331754cd9184f311affde0ce05520
Israel Ribeiro <israelriibeiro@gmail.com>
2014-06-16 11:08:34 -0300

    merge with master

--- manifests/environments/environment_nginx.pp
+++ manifests/environments/environment_nginx.pp
@@ -1,12 +1,11 @@
 class deploy_php::environments::environment_nginx {
 
-	package { [	"libpcre3", 
-							"php5-common", 
-							"libmysqlclient-dev",
-							"libmysqlclient18",
-							"php5-mcrypt"]:
-		   
+	package { [ "libpcre3", 
+		    "php5-common", 
+		    "libmysqlclient-dev",
+		    "libmysqlclient18",
+		    "php5-mcrypt"]:
 		ensure  => installed,
-  }
+	}
 
 }
